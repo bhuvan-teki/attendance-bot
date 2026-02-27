721f5e9b24a508d49753f1817fa68ce3f1ec6f44
Bhuvan Teki <bhuvanteki@gmail.com>
2026-02-27 06:50:24 +0000

    Integrated channel ID and schedules

--- bot.py
+++ bot.py
@@ -5,16 +5,17 @@ import os
 from flask import Flask
 from threading import Thread
 
-# 1. Tiny Web Server for Render
+# 1. Tiny Web Server to keep Render "awake"
 app = Flask('')
 @app.route('/')
 def home(): return "Attendance Bot is Online!"
 
 def run_web():
+    # Render uses port 10000 by default for web services
     app.run(host='0.0.0.0', port=10000)
 
 # 2. Discord Bot Setup
-# This line looks for your secret token in Render's settings
+# We use os.getenv so GitHub doesn't block your push for having a secret token
 TOKEN = os.getenv('MTQ3NjgxOTY5NjE4NjU1NjU1Nw.GSTHaw.tvqkv7mZV0QO3Si0np64RrnMkeErc__PCcoGcs') 
 CHANNEL_ID = 1476817434160926858  # Your Integrated Channel ID
 
@@ -22,7 +23,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# THE SCHEDULES (IST converted to UTC)
+# THE SCHEDULES (IST times converted to UTC for the server)
 @aiocron.crontab('30 03 * * *') # 9:00 AM IST
 async def t1(): await bot.get_channel(CHANNEL_ID).send("Attendance 9:00 AM @everyone")
 
@@ -51,7 +52,7 @@ async def t8(): await bot.get_channel(CHANNEL_ID).send("Attendance 3:40 PM @ever
 async def on_ready():
     print(f'Bot is online as {bot.user}')
 
-# 3. Start both
+# 3. Start both the Web Server and the Bot
 if __name__ == "__main__":
     Thread(target=run_web).start()
     bot.run(TOKEN)
